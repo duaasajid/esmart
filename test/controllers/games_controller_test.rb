195786152b6b8a438bf6f6de1f00a138534cabcf
apple <apple@apples-MacBook-Pro-3.local>
2023-08-27 14:25:22 -0700

    test cases added for match and games

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,13 +1,23 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
 
   test 'get index' do
-    skip 'todo'
+    user = create(:user)
+    sign_in user
+    get games_path
+    assert_response :success
+
+    assert_equal @games.count, user.games.count
   end
 
   test 'post add' do
-    skip 'todo'
+    user = create(:user)
+    sign_in user
+    game = create(:game)
+    post games_path, params: { game_id: game.id, handle: 'test_handle' }
+
+    assert_equal flash[:notice], 'The game has been added to your games.'
   end
 
 end
--- test/controllers/matches_controller_test.rb
+++ test/controllers/matches_controller_test.rb
@@ -1,9 +1,27 @@
-require 'test_helper'
+require 'test/test_helper'
 
 class MatchesControllerTest < ActionDispatch::IntegrationTest
 
-  test 'something' do
-    skip 'todo'
+  test 'index' do
+    user= create(:user)
+    opponent= create(:user)
+    sign_in user
+    game = create(:game)
+    match = create(:match, game: game, player1_id: user.id, player2_id: opponent.id)
+    get matches_path
+    assert_response :success
+
+    assert_equal @matches.first, match
+  end
+
+  test 'new' do
+    user = create(:user)
+    sign_in user
+    get new_match_path
+    assert_response :success
+    assert_equal @user_games, assigns(:user_games)
+
+    assert_equal @user_opponents, []
   end
 
 end
